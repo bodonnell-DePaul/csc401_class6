bc6eafb77a0cfd8aeef6e50732707f1738da7d02
Brian O'Donnell <bodonne3@depaul.edu>
2025-08-14 21:14:37 -0500

    getting there

--- BlackJack.py
+++ BlackJack.py
@@ -6,16 +6,15 @@ class BlackJack:
     def __init__(self):
         self.deck = Deck()
         self.deck.shuffle()
-        self.playerName = input('Enter your name')
+        self.playerName = input('Enter your name: ')
         self.houseDealer = 'Dealer'
         self.players = {self.playerName: [], self.houseDealer: []}
+        self.play()
 
-    def dealCards(self):
+    def dealCards(self, player):
         cnt = 0
         while cnt < 2 :
-            for p in self.players:
-                #temp = self.players[p]
-                self.players[p].append(self.deck.dealCard())
+            self.players[player].append(self.deck.dealCard())
             cnt += 1
 
     def calculateHand(self, playerHand):
@@ -31,34 +30,73 @@ class BlackJack:
 
         return retVal
 
-    def evaluate(self):
+    def evaluate(self, player):
         playerHand = 0
         dealerHand = 0
-        for p in self.players:
-            if p.lower().find('dealer') > -1:
-                playerHand = self.calculateHand(self.players[p])
-            else:
-                dealerHand = self.calculateHand(self.players[p])
+        gameOver = 0
+        if player.lower().find('dealer') > -1:
+            playerHand = self.calculateHand(self.players[player])
+        else:
+            dealerHand = self.calculateHand(self.players[player])
 
         if playerHand == 21:
             print('{} wins with a score of {}'.format(self.playerName, playerHand))
+            return gameOver+1
         elif playerHand > dealerHand and playerHand <= 21:
             print('{} wins with a score of {}'.format(self.playerName, playerHand))
             print('Dealer has a score of {}'.format(dealerHand))
+            return gameOver+1
         elif playerHand > 21:
             print('Dealer has a score of {}'.format(dealerHand))
+            return gameOver+1
         elif dealerHand > playerHand and dealerHand <= 21:
             print('Dealer has a score of {}'.format(dealerHand))
             print('{} lost with a score of {}'.format(self.playerName, playerHand))
+            return gameOver+1
+        
+        return gameOver
 
     
+    def playerChoice(self, choice, player):
+        if choice > 0:
+            self.players[player].append(self.deck.dealCard())
+
+    def play(self):
+
+        
+        
+        inGame = 0
+        playAgain = 'y'
+        while playAgain.lower().find('y') > -1:
+            for player in self.players:
+                if len(self.players[player]) == 0:
+                    self.dealCards(player)
+                print("{}'s hand is: {}".format(player, self.players[player]))
+                choice = 0
+                while True:
+                    try:
+                        choice = int(input('Enter 0 for Hit and 1 for Stay'))
+                        break
+                    except:
+                        print('You must enter 0 or 1')
+
+                self.playerChoice(choice, player)
+                inGame = self.evaluate(player)
+                if inGame > 1:
+                    playAgain = input('Do you want to play again? (Yes/No) ')
+                    break
+                print('Do you want to Hit or Stay?')
+                
+
+
+        
+
+        
 
 
 
 if __name__ == "__main__":
     game = BlackJack()
-    game.dealCards()
-    game.evaluate()
 #order of events
 # initiate BlackJack
 # Shuffle Deck
